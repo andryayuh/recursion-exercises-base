7324433902793ee49eca4cd4f24a6ed324fd6f0c
Andrea Miralles <andreamiralles@andreas-mbp.attlocal.net>
2017-05-12 00:04:50 -0700

    implement jsoninator

--- jsoninator.js
+++ jsoninator.js
@@ -7,7 +7,33 @@ const _ = require('underscore'); // the real one! :)
 
 
 const stringify = function(obj) {
-  // your code goes here
+  let arrayInput = [];
+  let vals = [];
+  let keys = [];
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+      return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        return '[]';
+      } else {
+          _.each(obj, el => arrayInput.push(stringify(el)));
+          return '[' + arrayInput + ']';
+      }
+    } else if (typeof obj === 'object') {
+        keys = Object.keys(obj);
+        _.each(keys, key => {
+          var keyString = '"' + key + '":';
+          var valString = obj[key];
+          if (typeof valString === 'string') {
+            vals.push(keyString + '"' + valString + '"');
+          } else if (typeof valString === 'object'){
+              vals.push(keyString + stringify(valString));
+          }
+        });
+        return '{' + vals + '}';
+    }   
 };
 
 module.exports = {
